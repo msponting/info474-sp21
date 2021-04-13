c25eebff1b0a44c8d680fffc16e9017fa84e0360
Michelle Ponting <michelleponting@Turnip.local>
2021-04-13 09:40:56 -0700

    add git ignore files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 const App = () => {
-    return ( <
-        div >
-        <
-        p > sad boi < /p> <
-        /div>
-    );
+  return (
+    <div>
+      <p>sad boi</p>
+    </div>
+  );
 };
 
 export default App;
